79875b192f9ebf0389359b88c8d52330d7edcb0d
monch1962 <monch1962@gmail.com>
2014-07-29 05:31:59 -0700

    Initial commit

---
+++ src/lib/fetch_market_data.js
@@ -0,0 +1,3 @@
+var fetch_market_data_url = "http://quiet-oasis-2159.herokuapp.com/pebblecard/code=";
+
+module.exports = fetch_market_data_url;
---
+++ src/lib/markets.js
@@ -0,0 +1,5 @@
+var markets = {
+  markets: ['FGBL4U.EX', 'TLS.AX']
+};
+
+module.exports = markets;
