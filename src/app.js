3852e07fc3283814dfce272b06efd61cdebf9962
monch1962 <monch1962@gmail.com>
2014-07-29 21:34:51 -0700

    Market menu now showing options correctly

--- src/app.js
+++ src/app.js
@@ -8,24 +8,23 @@ var UI = require('ui');
 var Vector2 = require('vector2');
 
 var main = new UI.Card({
-  title: 'Pebble.js',
+  title: 'Market Data',
   icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
+  //subtitle: 'Hello World!',
   body: 'Press any button.'
 });
 
 main.show();
 
-main.on('click', 'up', function(e) {
+main.on('click', 'select', function(e) {
   var menu = new UI.Menu({
     sections: [{
       items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
+        title: 'FGBL4U.EX',
+        subtitle: 'FGBL Sep14'
       }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
+        title: 'TLS.AX',
+        subtitle: 'Telstra'
       }]
     }]
   });
@@ -35,23 +34,23 @@ main.on('click', 'up', function(e) {
   menu.show();
 });
 
-main.on('click', 'select', function(e) {
-  var wind = new UI.Window();
-  var textfield = new UI.Text({
-    position: new Vector2(0, 50),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
-});
+//main.on('click', 'select', function(e) {
+//  var wind = new UI.Window();
+//  var textfield = new UI.Text({
+//    position: new Vector2(0, 50),
+//    size: new Vector2(144, 30),
+//    font: 'gothic-24-bold',
+//    text: 'Text Anywhere!',
+//    textAlign: 'center'
+//  });
+//  wind.add(textfield);
+//  wind.show();
+//});
 
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
-});
+//main.on('click', 'down', function(e) {
+//  var card = new UI.Card();
+//  card.title('A Card');
+//  card.subtitle('Is a Window');
+//  card.body('The simplest window type in Pebble.js.');
+//  card.show();
+//});
